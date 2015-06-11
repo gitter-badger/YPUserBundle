9a5d983fd99190bde1cb23a76102bf1913ef0a52
Prou Yann <yprou@inouit.com>
2015-06-11 10:04:08 +0200

    [upd] ToofffUserExtension for configuration

--- DependencyInjection/ToofffUserExtension.php
+++ DependencyInjection/ToofffUserExtension.php
@@ -2,6 +2,7 @@
 
 namespace Toofff\UserBundle\DependencyInjection;
 
+use Symfony\Bundle\FrameworkBundle\DependencyInjection\Configuration;
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
@@ -13,21 +14,34 @@ use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
  */
 class ToofffUserExtension extends Extension
 {
+    /**
+     * Responds to the app.config configuration parameter.
+     *
+     * @param array            $configs
+     * @param ContainerBuilder $container
+     *
+     * @throws LogicException
+     */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $processor = new Processor();
-        $configuration = new Configuration();
-        $config = $processor->processConfiguration($configuration, $configs);
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        foreach (array('services') as $basename) {
-            $loader->load(sprintf('%s.xml', $basename));
-        }
+        // foreach (array('services') as $basename) {
+        //     $loader->load(sprintf('%s.xml', $basename));
+        // }
 
+        $processor = new Processor();
+        $configuration = new Configuration(false);
+        $config = $processor->processConfiguration($configuration, $configs);
     }
 
+    /**
+     * Returns the base path for the XSD files.
+     *
+     * @return string The XSD base path
+     */
     public function getXsdValidationBasePath()
     {
-        return __DIR__.'/../Resources/config/';
+        return __DIR__.'/../Resources/config/schema';
     }
 
     public function getNamespace()
