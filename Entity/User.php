beef4927a1cb6082f1017cfdc67959d2dd273324
Prou Yann <prouyann@gmail.com>
2015-06-13 09:55:02 +0200

    [upd] Entity Abstract User with ORM Comment

--- Entity/User.php
+++ Entity/User.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace YP\UserBundle\Model;
+namespace YP\UserBundle\Entity;
 
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @author Prou Yann <prouyann@gmail.com>
@@ -12,53 +13,87 @@ abstract class User
 {
     /**
      * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", length=30)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=255)
      */
     protected $username;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="username_canonical", type="string", length=255, unique=true)
      */
     protected $usernameCanonical;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
      */
     protected $email;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="email_canonical", type="string", length=255, unique=true)
      */
     protected $emailCanonical;
 
     /**
      * @var boolean
+     *
+     * @ORM\Column(name="enabled", type="boolean")
      */
     protected $enabled;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
      */
     protected $password;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="plain_password", type="string", length=255)
      */
     protected $plainPassword;
 
     /**
      * @var \DateTime
+     *
+     * @ORM\Column(name="last_login", type="datetime", nullable=true)
      */
     protected $lastLogin;
 
     /**
      * @var array
+     *
+     * @ORM\Column(name="roles", type="array")
      */
     protected $roles;
+
+
+    /**
+     * __construct
+     *
+     * @return string
+     */
+    public function __construct()
+    {
+        $this->enabled = false;
+        $this->roles = array();
+    }
 
     /**
      * Get id
@@ -229,6 +264,37 @@ abstract class User
     public function getLastLogin()
     {
         return $this->lastLogin;
+    }
+
+    /**
+     * Set roles
+     *
+     * @param Array $roles
+     * @return User
+     */
+    public function setRoles(array $roles)
+    {
+        $this->roles = array();
+
+        foreach ($roles as $role) {
+            $role = strtoupper($role);
+
+            if (!in_array($role, $this->roles, true)) {
+                $this->roles[] = $role;
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get roles
+     *
+     * @return array
+     */
+    public function getRoles()
+    {
+        return array_unique($this->roles);
     }
 
     /**
